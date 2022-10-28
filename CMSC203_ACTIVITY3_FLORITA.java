badac53870024e40f642d29032b2afc40b769c38
Dennis1903 <112455243+Dennis1903@users.noreply.github.com>
2022-10-27 19:27:25 -0700

    Add files via upload

---
+++ CMSC203_ACTIVITY3_FLORITA.java
@@ -0,0 +1,65 @@
+package UberGrab;
+ 
+import java.util.Scanner;
+
+public class CMSC203_ACTIVITY3_FLORITA {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        boolean done = false;
+        
+        do{
+            System.out.print( "No. of test cases: ");
+            int testCases = scanner.nextInt();
+            
+            if (1 <= testCases && testCases <= 10){
+                String inputs[] = new String[testCases];
+                for (int i = 0; i < testCases; i++) {
+                    scanner = new Scanner(System.in);
+                    inputs[i] = scanner.nextLine();
+                }
+                
+                System.out.println();
+                System.out.println( "Result:");
+                System.lineSeparator();
+                for (String input : inputs)
+                    System.out.println(processInput(input));
+                    
+                    
+                done = true;
+            } else
+                System.out.println( "Enter numbers 1-10 only!");
+                
+        } while (!done);
+        scanner.close();
+    } 
+    
+    private static String processInput(String input) {
+        String[] inputs = input.split( "\\s+");
+        
+        if (inputs.length != 2)
+            return "Invalid Input";
+            
+            
+        try {
+            int uber = Integer.parseInt(inputs[0]);
+            int grab = Integer.parseInt(inputs[1]);
+            
+            if (uber < 1 || uber > 1000 
+                    || grab < 1 || grab > 1000)
+                return "Invalid Input";
+                
+            if (uber < grab)
+                return "First";
+            
+            else if (uber > grab)
+                return "Second";
+                
+            else
+                return "Any";
+        } catch (Exception e) {
+            return "Invalid Input";
+        }
+    }    
+}
+
